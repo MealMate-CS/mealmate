392b8b02f99d6250acaa8289e7da4c8e7880f2fd
unknown <sungdo1004@gmail.com>
2019-12-30 16:09:56 -0800

    react-router template

---
+++ src/container/LoginContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LoginContainer extends React.Component {
+    render() {
+        return(
+            <div>
+                <p>
+                    This LoginContainer
+                </p>
+            </div>
+        )
+    }
+
+}
+
+export default LoginContainer;
