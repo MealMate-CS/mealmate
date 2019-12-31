e84c255e03ab07b300579282aa08dc00b564c995
Jourdan Ball <jourdan.ball@gmail.com>
2019-12-31 12:05:18 -0800

    Middleware constructed for Consumers

--- server/controller/consumerController.js
+++ server/controller/consumerController.js
@@ -1,2 +1,25 @@
-const consumer ={};
+const consumerController ={};
+const pool = require('../database/database')
+
+consumerController.recentHistory = (req, res, next) => {
+    const username = req.body.username
+    pool.query('SELECT Consumer.history FROM User, Consumer WHERE User.$1 = Consumer.username', [username], (error, results) => {
+        if(error){
+            throw error;
+        }
+        res.status(200).json(results.rows)
+    })
+}
+
+consumerController.createReservation = (req, res, next) => {
+    const restaurant = req.body.restaurant;
+    const createdAt = req.body.createdAt;
+    const timeFrame = req.body.timeFrame
+    pool.query('INSERT INTO Consumers (history) VALUES ($1, $2, $3)', [restaurant, createdAt, timeFrame], (error, results) => {
+        if(error) {
+            throw error
+        }
+        res.status(200).send(`Reservation set for ${restaurant} from ${timeFrame}`)
+    })
+}
 module.exports = consumerController;
