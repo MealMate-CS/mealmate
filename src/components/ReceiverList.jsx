e33639b493bac78d0436ed9aa9fc9e3a4e95df94
Stephanie Chiu <stephaniekchiu@gmail.com>
2020-01-01 23:29:19 -0800

    update Map component, start ReceiverList, and implement conditional renderng

---
+++ src/components/ReceiverList.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+const ReceiverList = () => {
+
+  const [donors, setDonors] = useState([])
+
+  //grab list of donors
+  useEffect(() => {
+    fetch('/getDonors')
+    .then(res => res.json())
+    .then(data => setDonors(data))
+    .catch(err => console.log(err))
+  })
+
+  const list = donors.map(() => {
+    
+  })
+
+  return(
+    <div className='receiver-list'>
+      List
+    </div>
+  )
+}
+
+export default ReceiverList;
