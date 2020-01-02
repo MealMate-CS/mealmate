3a1e987f9597ce66b9775b40cc12c4af4601dc68
Jourdan Ball <jourdan.ball@gmail.com>
2020-01-01 17:26:19 -0800

    user login and signup code from Kelvin

--- server/controller/consumerController.js
+++ server/controller/consumerController.js
@@ -15,7 +15,7 @@ consumerController.createReservation = (req, res, next) => {
     const restaurant = req.body.restaurant;
     const createdAt = req.body.createdAt;
     const timeFrame = req.body.timeFrame;
-    pool.query('INSERT INTO Consumers (history) VALUES ($1, $2, $3)', [restaurant, createdAt, timeFrame], (error, results) => {
+    pool.query('INSERT INTO History VALUES ($1, $2, $3)', [restaurant, createdAt, timeFrame], (error, results) => {
         if(error) {
             throw error
         }
--- server/routers/dbRouter.js
+++ server/routers/dbRouter.js
@@ -2,5 +2,14 @@ const express = require('express');
 const router = express.Router();
 const dbController = require('../controller/dbController');
 
+router.post('/donorSignUp',dbController.donorSignUp,(req,res)=>{
+    res.sendStatus(200);
+})
+router.post('/receiverSignUp', dbController.receiverSignUp,(req,res)=>{
+    res.sendStatus(200);
+})
+router.get('/userLogin',dbController.checkLogin,(req,res)=>{
+    res.sendStatus(200);
+})
 
 module.exports = router;
