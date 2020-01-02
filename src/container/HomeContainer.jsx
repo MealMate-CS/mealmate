d44096f5edec8eaaafc58045aa5b46ada68e7332
unknown <sungdo1004@gmail.com>
2020-01-01 18:57:29 -0800

    intial stage to merge backend with frontend

--- src/container/HomeContainer.jsx
+++ src/container/HomeContainer.jsx
@@ -2,26 +2,36 @@ import React from 'react'
 import LoginContainer from './LoginContainer.jsx'
 import SignupAsDonorContainer from './SignupAsDonorContainer.jsx'
 import SignupAsReceiverContainer from './SignupAsReceiverContainer.jsx'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-  } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-class HomeContainer extends React.Component {
-    render() {
-        return (
-            <Router>
-                <div id='home-container'>
-                    <Switch>
-                        <Route path='/' exact component={LoginContainer} />
-                        <Route path='/signupasdonor' component={SignupAsDonorContainer} />
-                        <Route path='/signupasreceiver' component={SignupAsReceiverContainer} />                        
-                    </Switch>
-                </div>
-            </Router>
-        )
-    }
+const HomeContainer = (props) => {
+    return (
+        <Router>
+            <div id='home-container'>
+                <Route path='/' exact render={()=> <LoginContainer password={props.password}
+                userType={props.userType} userId={props.userId} isLoggedInAsDonor={props.isLoggedInAsDonor}
+                isLoggedInasReceiver={props.isLoggedInasReceiver}
+                setLoginId={props.setSignupId} setLoginPassword={props.setSignupPassword}
+                loginSubmit={props.loginSubmit} errorMessage={props.errorMessage}
+                />}/>
+                <Route path='/signupasdonor' render={() => <SignupAsDonorContainer 
+                newUserId={props.newUserId} newPassword={props.newPassword} errorMessage={props.errorMessage}
+                newUserPhoneNumber={props.newUserPhoneNumber} newUserOrganization={props.newUserOrganization} 
+                newUserAddress={props.newUserAddress} setSignupId={props.setSignupId} 
+                setPhoneNumber={props.setPhoneNumber} setSignupOrganization={props.setSignupOrganization}
+                setAddress={props.setAddress} 
+                setSignupPassword={props.setSignupPassword} donorSignupSubmit={props.donorSignupSubmit}
+                />} />
+                <Route path='/signupasreceiver' render={() => <SignupAsReceiverContainer 
+                newUserId={props.newUserId} newPassword={props.newPassword}
+                errorMessage={props.errorMessage} newUserPhoneNumber={props.newUserPhoneNumber}
+                setSignupId={props.setSignupId} setSignupPassword={props.setSignupPassword}
+                setPhoneNumber={props.setPhoneNumber} receiverSignupSubmit={props.receiverSignupSubmit}
+                />}/>
+            </div>
+        </Router>
+    )
 }
+
 
 export default HomeContainer
--- src/container/LoginContainer.jsx
+++ src/container/LoginContainer.jsx
@@ -1,79 +1,23 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-class LoginContainer extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            userId : '',
-            password : '',
-            userType : '',
-            errorMessage : '',
-            isLoggedInAsDonor: false,
-            isLoggedInAsReceiver: false
-        }
-
-    // this.loginButton = this.loginButton.bind(this)    
-    this.setLoginId = this.setLoginId.bind(this)
-    this.setLoginPassword  = this.setLoginPassword.bind(this)
-    this.loginSubmit = this.loginSubmit.bind(this)
-    }
-
-    setLoginId(event) {
-        this.setState({userId:event.target.value})
-        console.log('event in setloginid', event.target.value)
-    }
-    setLoginPassword(event) {
-        this.setState({password:event.target.value})
-        console.log('event in setLoginPassword', event.target.value)
-    }
-    loginSubmit() {
-        const body = {
-            userId : this.state.userId,
-            password : this.state.password
-        }
-        fetch('/userlogin', {
-            method: 'GET', 
-            body: JSON.stringify(body), 
-            headers : {'Content-Type': 'application/json'}
-        })
-        .then(data => data.json())
-        .then(result => {
-            console.log('result in loginSubmit =>', result)
-        })
-        .catch(err => {
-            console.log(err)
-            this.setState({errorMessage: `${JSON.stringify(err)}`})
-        })
-    }
-
-    
-
-    render() {
-        return(
-            <div id="user-login-container">
-                <section id="user-login-input">
-                    <input type='input' id='login-username' placeholder='Your Username' onChange={this.setLoginId} required />
-                    <input type='password' id='login-password' placeholder='Your Password' onChange={this.setLoginPassword} required />
-                    <input type='button' id='login-button' value='Log In' onClick={this.loginSubmit}/>
-                    {this.state.errorMessage}
-                    <Link to='/signupasreceiver'>
-                        <input type='button' id='receiver-signup-button' value='Sign Up As Receiver'></input>
-                    </Link>
-                    <Link to='/signupasdonor'>
-                        <input type='button' id='donor-signup-button' value='Sign Up as Donor'></input>
-                    </Link>
-                </section>
-            </div>
-        )
-    }
-
+const LoginContainer = (props) => {
+    return(
+        <div id="user-login-container">
+            <section id="user-login-input">
+                <input type='input' id='login-username' placeholder='Your Username' onChange={props.setLoginId} required />
+                <input type='password' id='login-password' placeholder='Your Password' onChange={props.setLoginPassword} required />
+                <input type='button' id='login-button' value='Log In' onClick={props.loginSubmit}/>
+                {props.errorMessage}
+                <Link to='/signupasreceiver'>
+                    <input type='button' id='receiver-signup-button' value='Sign Up As Receiver'></input>
+                </Link>
+                <Link to='/signupasdonor'>
+                    <input type='button' id='donor-signup-button' value='Sign Up as Donor'></input>
+                </Link>
+            </section>
+        </div>
+    )
 }
 
 export default LoginContainer;
